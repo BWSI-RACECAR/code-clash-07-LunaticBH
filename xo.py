a0d3e422993a8b63858cc350dbcf465bdfde435c
Enzo C <125571218+LunaticBH@users.noreply.github.com>
2023-07-20 09:06:02 -0400

    Add files via upload

---
+++ xo.py
@@ -0,0 +1,62 @@
+"""
+Copyright MIT BWSI Autonomous RACECAR Course
+MIT License
+Summer 2023
+
+Code Clash #7 - X’s and O’s (xo.py)
+
+
+Author: Andrew Scott White
+
+Difficulty Level: 5/10
+
+Prompt: Jonas and Shraddha are playing a card game, 
+but Jonas keeps cheating by drawing extra cards. Shraddha
+bought a device that tracks how many cards are drawn and who took them. 
+She has asked you to write a script to ensure he isn’t cheating. The device
+stores Jonas’ draws as X and Shraddha’s as O. 
+
+Create a Python function that accepts a string. This 
+function should count the number of Xs and the number of Os in the string.
+It should then return a boolean value of either True or False. If the count of
+Xs and Os are equal, then the function should return True. Otherwise, the function 
+should return False.
+
+Test Cases:
+Input: XXXO Output: False
+
+Input: XXXOOO Output: True
+
+Input: xO Output: False
+
+"""
+
+
+class Solution:
+    def count_XO(self, string):
+            #type string: string
+            #return type: boolean
+            
+            string = string.upper()
+            xs = 0
+            os = 0
+            for i in range(len(string)):
+                if string[i] == "X":
+                    xs +=1
+                elif string[i] == "0":
+                    os += 1
+            if xs == os:
+                return True
+            else:
+                return False
+            pass
+                
+def main():
+    input1=input()
+    tc1 = Solution()
+    ans = tc1.count_XO(input1)
+    print(ans)
+
+if __name__ == "__main__":
+    main()
+    
